c643f065941e6f054f6fec766169c2db8aa3ddbd
flahn <florian.lahn@googlemail.com>
2015-07-06 12:15:27 +0200

    Update database.js
    ...

--- config/database.js
+++ config/database.js
@@ -1,5 +1,3 @@
 module.exports = {
 
-    //'url' : 'mongodb://web_user:DirtyLittlePWD@serverzwerg:27017/users'
-	'url' : 'mongodb://web_user:Passwort_fuer_DB@giv-mgl.uni-muenster.de:27017/users' 
 };
